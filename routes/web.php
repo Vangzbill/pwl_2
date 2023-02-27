4bb0e3bd52bb18deef16fafd82bbe03f73a861f9
Sabilla Luthfi Rahmadhan <mamadhan017@gmail.com>
2023-02-27 09:53:51 +0700

    Minggu 3/Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/about', [AboutController::class, 'index']);
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get('/about', [AboutController::class, 'index']);
+// Route::get('/articles/{id}', [ArticleController::class, 'show']);
+
+// Route::get('/', [PageController::class, 'home']);
+
+// Route::prefix('products')->group(function(){
+//     Route::get('/list', [PageController::class, 'products']);
+// });
+
+// Route::get('/news/{key}', [PageController::class, 'news']);
+
+// Route::prefix('programs')->group(function(){
+//     Route::get('/list', [PageController::class, 'programs']);
+// });
+
+// Route::get('/about', [PageController::class, 'about']);
+
+// Route::resource('contact', PageController::class);
+
+Route::get('/', [HomeController::class, 'home']);
+
+Route::prefix('product')->group(function(){
+    Route::get('/list', [HomeController::class, 'product']);
+});
+
+Route::get('/news/{key}', [HomeController::class, 'news']);
+
+Route::prefix('programs')->group(function(){
+    Route::get('/list', [HomeController::class, 'programs']);
+});
+
+Route::get('/about', [HomeController::class, 'about']);
+
+Route::resource('contact', HomeController::class);
